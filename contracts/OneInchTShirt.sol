e433679664abf78bc44ed6816baa237fc353fee7
Mikhail Melnik <by.zumzoom@gmail.com>
2021-02-17 14:15:48 +0300

    add rescueFunds

--- contracts/OneInchTShirt.sol
+++ contracts/OneInchTShirt.sol
@@ -3,10 +3,19 @@
 pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract OneInchTShirt is ERC20Burnable {
+contract OneInchTShirt is ERC20Burnable, Ownable {
+    using SafeERC20 for IERC20;
+
     constructor(address _owner) ERC20("1INCH T-Shirt", "T-SHIRT") {
         _mint(_owner, 254 ether);
+        transferOwnership(_owner);
+    }
+
+    function rescueFunds(IERC20 token, uint256 amount) external onlyOwner {
+        token.safeTransfer(msg.sender, amount);
     }
 }
